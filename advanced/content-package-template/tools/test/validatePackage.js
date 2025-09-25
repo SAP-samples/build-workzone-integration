28fb88f31dbdd4246a8300b7c8bcc88bc0caf55f
Joyce Ji <joyce.ji01@sap.com>
2025-09-25 10:07:17 +0800

    remove tools and change sap-workzone-cpkg-tools reference

--- advanced/content-package-template/tools/test/validatePackage.js
+++
@@ -1,6 +0,0 @@
-var validate = require("../contentpackage/validate"),
-	path = require("path");
-
-var dir = process.argv[2].split("/");
-dir = __dirname.split("/").concat(dir);
-validate.validate("/" + path.join.apply(path, dir));
